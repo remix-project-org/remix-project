61a5054f53dbd4bf7df6f7fb681209175266cbd1
Aniket-Engg <aniketengg.225@gmail.com>
2026-01-09 17:35:47 +0530

    fix e2e

--- apps/remix-ide-e2e/src/tests/terminal.test.ts
+++ apps/remix-ide-e2e/src/tests/terminal.test.ts
@@ -66,6 +66,8 @@ module.exports = {
 
   'Call Remix File Manager from a script #group2': function (browser: NightwatchBrowser) {
     browser
+      .click('*[data-id="toggleBottomPanelIcon"]')
+      .waitForElementVisible('.terminal-wrap', 5000)
       .addFile('asyncAwaitWithFileManagerAccess.js', { content: asyncAwaitWithFileManagerAccess })
       .executeScriptInTerminal('remix.execute(\'asyncAwaitWithFileManagerAccess.js\')')
       .waitForElementContainsText('*[data-id="terminalJournal"]', 'contract Ballot {', 60000)
@@ -73,16 +75,21 @@ module.exports = {
 
   'Call web3.eth.getAccounts() using Remix VM #group2': function (browser: NightwatchBrowser) {
     browser
+      // Terminal should already be visible from "Call Remix File Manager from a script" test
       .executeScriptInTerminal('web3.eth.getAccounts()')
       .waitForElementContainsText('*[data-id="terminalJournal"]', '["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db","0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB","0x617F2E2fD72FD9D5503197092aC168c91465E7f2","0x17F6AD8Ef982297579C203069C1DbfFE4348c372","0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678","0x03C6FcED478cBbC9a4FAB34eF9f40767739D1Ff7","0x1aE0EA34a72D944a8C7603FfB3eC30a6669E454C","0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC","0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c","0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C","0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB","0x583031D1113aD414F02576BD6afaBfb302140225","0xdD870fA1b7C4700F2BD7f44238821C26f7392148"]')
   },
 
   'Call web3.eth.getAccounts() using External Http Provider #group5': function (browser: NightwatchBrowser) {
     browser
-      .click('*[data-id="terminalClearConsole"]') // clear  the terminal
+      .click('*[data-id="toggleBottomPanelIcon"]')
+      .waitForElementVisible('.terminal-wrap', 5000)
       .clickLaunchIcon('udapp')
       .switchEnvironment('basic-http-provider')
       .modalFooterOKClick('basic-http-provider')
+      .waitForElementPresent('[data-id="selected-provider-basic-http-provider"]', 15000) // Wait for provider to be selected
+      .pause(2000) // Wait for environment switch to fully complete
+      .click('*[data-id="terminalClearConsole"]') // clear  the terminal
       .executeScriptInTerminal('web3.eth.getAccounts()')
       .waitForElementContainsText('*[data-id="terminalJournal"]', '["', 60000) // we check if an array is present, don't need to check for the content
       .waitForElementContainsText('*[data-id="terminalJournal"]', '"]', 60000)
@@ -91,6 +98,8 @@ module.exports = {
 
   'Call Remix File Resolver (external URL) from a script #group3': function (browser: NightwatchBrowser) {
     browser
+      .click('*[data-id="toggleBottomPanelIcon"]')
+      .waitForElementVisible('.terminal-wrap', 5000)
       .click('*[data-id="terminalClearConsole"]') // clear the terminal
       .addFile('resolveExternalUrlAndSave.js', { content: resolveExternalUrlAndSave })
       .openFile('resolveExternalUrlAndSave.js')
@@ -131,6 +140,8 @@ module.exports = {
       .click('*[data-id="topbar-settingsIcon"]')
       .clickLaunchIcon('udapp')
       .switchEnvironment('vm-london')
+      .click('*[data-id="toggleBottomPanelIcon"]')
+      .waitForElementVisible('.terminal-wrap', 5000)
       .click('*[data-id="terminalClearConsole"]') // clear the terminal
       .clickLaunchIcon('filePanel')
       .click('*[data-id="treeViewUltreeViewMenu"]') // make sure we create the file at the root folder
