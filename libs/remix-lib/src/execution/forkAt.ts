b8379549bebf80a85b18f6ab69f9b40c24d467e9
operagxoksana <operagxoksana@gmail.com>
2026-03-05 17:40:09 +0200

    docs: fix JSDoc types in forkAt

--- libs/remix-lib/src/execution/forkAt.ts
+++ libs/remix-lib/src/execution/forkAt.ts
@@ -3,8 +3,8 @@
 /**
   * returns the fork name for the @argument networkId and @argument blockNumber
   *
-  * @param {Object} networkId - network Id (1 for VM, 3 for Ropsten, 4 for Rinkeby, 5 for Goerli)
-  * @param {Object} blockNumber - block number
+  * @param {number} networkId - network Id (1 for VM, 3 for Ropsten, 4 for Rinkeby, 5 for Goerli)
+  * @param {number} blockNumber - block number
   * @return {String} - fork name (Berlin, Istanbul, ...)
   */
 export function forkAt (networkId, blockNumber) {
