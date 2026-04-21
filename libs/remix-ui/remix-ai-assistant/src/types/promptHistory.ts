79a8f15978cb8e903875cb946bd54152b81642eb
Joseph Izang <josephizang@gmail.com>
2026-04-21 09:58:56 +0100

    create structure for prompt history

---
+++ libs/remix-ui/remix-ai-assistant/src/types/promptHistory.ts
@@ -0,0 +1,23 @@
+
+class HistoryCircularButffer<T> {
+  buffer: T[] = []
+
+  maxLength: number = 10
+
+  enqueue() : void {
+
+  }
+
+  dequeue() : T {
+    return {} as T;
+  }
+
+  firstOrHead() : T {
+    return {} as T;
+  }
+
+  lastOrTail() : T {
+    return {} as T;
+  }
+
+}
