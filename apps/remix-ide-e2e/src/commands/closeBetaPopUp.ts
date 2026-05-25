6028e5834e064ac66b9fbf143910552f12221f6c
Aniket-Engg <aniketengg.225@gmail.com>
2026-05-25 19:00:20 +0530

    fix popup

--- apps/remix-ide-e2e/src/commands/closeBetaPopUp.ts
+++ apps/remix-ide-e2e/src/commands/closeBetaPopUp.ts
@@ -8,17 +8,23 @@ class CloseBetaPopUp extends EventEmitter {
     this: NightwatchBrowser,
   ): NightwatchBrowser {
     this.api
-      .isVisible({ selector: "[data-id='beta-corner-widget']", suppressNotFoundErrors: true, timeout: 1000 }, (result) => {
-        if (result.value) {
-          browser.execute(function () {
-            const closeIcon = document.querySelector('.beta-corner-widget-close') as HTMLElement
+      .perform((done: () => void) => {
+        this.api.isVisible({ selector: "[data-id='beta-corner-widget']", suppressNotFoundErrors: true, timeout: 1000 }, (result) => {
+          if (result.value) {
+            this.api.execute(function () {
+              const closeIcon = document.querySelector('.beta-corner-widget-close') as HTMLElement
 
-            if (closeIcon) {
-              closeIcon.scrollIntoView({ behavior: 'auto', block: 'center' })
-              closeIcon.click()
-            }
-          }, [])
-        }})
+              if (closeIcon) {
+                closeIcon.scrollIntoView({ behavior: 'auto', block: 'center' })
+                closeIcon.click()
+              }
+            }, [])
+              .perform(() => done())
+          } else {
+            done()
+          }
+        })
+      })
       .perform(() => {
         this.emit('complete')
       })
